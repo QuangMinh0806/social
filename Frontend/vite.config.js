ae4b7c1232ef65002578ce3f2375a7eaad146a17
HTTTdh <thuthanh0501@gmail.com>
2025-10-20 21:37:55 +0700

    sua ten mien

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     ],
     proxy: {
       '/api': {
-        target: 'https://autosocial.tmedu.vn/api',
+        target: '/api',
         changeOrigin: true,
       },
     },
